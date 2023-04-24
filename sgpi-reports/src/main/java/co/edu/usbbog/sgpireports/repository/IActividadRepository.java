796fc4885e2e7fb694aec46095a3a9f698cf59c9
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2023-04-24 16:39:59 -0500

    Delete sgpi-reports directory

--- sgpi-reports/src/main/java/co/edu/usbbog/sgpireports/repository/IActividadRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.usbbog.sgpireports.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import co.edu.usbbog.sgpireports.model.Actividad;
-
-
-interface IActividadRepository extends JpaRepository<Actividad, String> {
-
-}
--- sgpi-reports/src/main/java/co/edu/usbbog/sgpireports/repository/IDetalleConvocatoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.usbbog.sgpireports.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.usbbog.sgpireports.model.DetalleConvocatoria;
-
-public interface IDetalleConvocatoriaRepository extends JpaRepository<DetalleConvocatoria, Integer>{
-
-}
