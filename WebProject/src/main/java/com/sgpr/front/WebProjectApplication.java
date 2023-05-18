89298869b260ba11e1e1d7874e099aef3a8b4f5a
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2023-05-18 12:14:50 -0500

    Update WebProjectApplication.java

--- WebProject/src/main/java/com/sgpr/front/WebProjectApplication.java
+++ WebProject/src/main/java/com/sgpr/front/WebProjectApplication.java
@@ -8,6 +8,7 @@ public class WebProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebProjectApplication.class, args);
+		System.out.println("Prueba 1");
 	}
 
 }
