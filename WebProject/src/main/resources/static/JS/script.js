efa5c711e209edd6aed5496bc89930853e61fc7a
jdgonzalezd1 <60698448+jdgonzalezd1@users.noreply.github.com>
2023-05-19 15:58:30 -0500

    Mod Front 3

--- WebProject/src/main/resources/static/JS/script.js
+++ WebProject/src/main/resources/static/JS/script.js
@@ -1,27 +1,27 @@
 function displayProyectos(){
-    document.getElementById("menuI").setAttribute("src","/Html/inicio/proyectos.html");
+    document.getElementById("menuI").setAttribute("src","/inicio/proyectos.html");
 }
 
 function displayGrupos(){
-    document.getElementById("menuI").setAttribute("src","/Html/inicio/grupos_investigacion.html");
+    document.getElementById("menuI").setAttribute("src","/inicio/grupos_investigacion.html");
 }
 
 function displaySemilleros(){
-    document.getElementById("menuI").setAttribute("src","/Html/inicio/semilleros.html");
+    document.getElementById("menuI").setAttribute("src","/inicio/semilleros.html");
 }
 
 function menuGrupos(){
-    document.getElementById("menuS").setAttribute("src","/Html/menu_sistema/grupos_investigacion/modulos_gi.html")
+    document.getElementById("menuS").setAttribute("src","/menu_sistema/grupos_investigacion/modulos_gi.html")
 }
 
 function menuSemilleros(){
-    document.getElementById("menuS").setAttribute("src","/Html/menu_sistema/semilleros/modulos_sem.html")
+    document.getElementById("menuS").setAttribute("src","/menu_sistema/semilleros/modulos_sem.html")
 }
 
 function menuProyectos(){
-    document.getElementById("menuS").setAttribute("src","/Html/menu_sistema/proyectos/modulos_proy.html")
+    document.getElementById("menuS").setAttribute("src","/menu_sistema/proyectos/modulos_proy.html")
 }
 
 function menuUsuarios(){
-    document.getElementById("menuS").setAttribute("src","/Html/menu_sistema/gestion_usuarios/modulo_usuarios.html")
+    document.getElementById("menuS").setAttribute("src","/menu_sistema/gestion_usuarios/modulo_usuarios.html")
 }
